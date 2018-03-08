4344998479db4c1699f0dee7379038c2e8afef9d
jgoerner <joshua.goerner@gmail.com>
2018-03-08 18:51:35 +0100

    implemented odd tranformer

--- MarchMadness/src/transformer.py
+++ MarchMadness/src/transformer.py
@@ -10,7 +10,7 @@ os.chdir("/home/jovyan/work")
 import pandas as pd
 from sklearn.base import TransformerMixin
 
-from src.utils import get_table
+from src.utils import get_table, calc_odds
 
 
 class WinLossTransformer(TransformerMixin):
@@ -48,7 +48,7 @@ class WinLossTransformer(TransformerMixin):
             right_on=["season", "team_id"],
         )[win_cols]
         # suffix to 'a' columns
-        df_merged_wins_a.columns = map(lambda c: c + "_a", df_merged_wins_a.columns)
+        df_merged_wins_a.columns = map(lambda c: c + "_reg_a", df_merged_wins_a.columns)
         # merge team b
         df_merged_wins_b = pd.merge(
             X,
@@ -58,7 +58,7 @@ class WinLossTransformer(TransformerMixin):
             right_on=["season", "team_id"],
         )[win_cols]
         # suffix to 'b' columns
-        df_merged_wins_b.columns = map(lambda c: c + "_b", df_merged_wins_b.columns)
+        df_merged_wins_b.columns = map(lambda c: c + "_reg_b", df_merged_wins_b.columns)
         df_merged_wins = pd.merge(
             df_merged_wins_a,
             df_merged_wins_b,
@@ -89,7 +89,7 @@ class WinLossTransformer(TransformerMixin):
             right_on=["season", "team_id"],
         )[loss_cols]
         # suffix to 'a' columns
-        df_merged_losses_a.columns = map(lambda c: c + "_a", df_merged_losses_a.columns)
+        df_merged_losses_a.columns = map(lambda c: c + "_reg_a", df_merged_losses_a.columns)
         # merge team b
         df_merged_losses_b = pd.merge(
             X,
@@ -99,7 +99,7 @@ class WinLossTransformer(TransformerMixin):
             right_on=["season", "team_id"],
         )[loss_cols]
         # suffix to 'b' columns
-        df_merged_losses_b.columns = map(lambda c: c + "_b", df_merged_losses_b.columns)
+        df_merged_losses_b.columns = map(lambda c: c + "_reg_b", df_merged_losses_b.columns)
         df_merged_losses = pd.merge(
             df_merged_losses_a,
             df_merged_losses_b,
@@ -121,3 +121,51 @@ class WinLossTransformer(TransformerMixin):
         )
         
         return df_final
+    
+    
+class OddTransformer(TransformerMixin):
+    """TODO: Docstring here"""
+    
+    def __init__(self, cols_wins_a, cols_losses_a, cols_wins_b, cols_losses_b, n_samples=100, n_experiments=100):
+        # check every param instance of list
+        self.cols_wins_a = cols_wins_a
+        self.cols_losses_a = cols_losses_a
+        self.cols_wins_b = cols_wins_b
+        self.cols_losses_b = cols_losses_b
+        self.n_samples = n_samples
+        self.n_experiments = n_experiments
+        
+    def fit(self, X):
+        # check if dataframe
+        if not isinstance(X, pd.DataFrame):
+            raise ValueError("Must pass a pandas DataFrame, got {}".format(type(X)))
+        
+        # check if cols for calc_odds are already given
+        odd_cols = set(["wins_a", "losses_a", "wins_b", "losses_b"]).intersection(set(X.columns))
+        if len(odd_cols) > 0 : 
+            raise ValueError("Please rename the following columns: {}".format(odd_cols))
+        
+        # check if specified cols are in df
+        col_diff = set(
+            self.cols_wins_a +
+            self.cols_losses_a +
+            self.cols_wins_b +
+            self.cols_losses_b
+        ) - set(X.columns)
+        if len(col_diff) > 0:
+            raise ValueError(
+                "Specified columns can't be found in provided DataFrame: {}".format(col_diff))
+        return self
+    
+    def transform(self, X):
+        # merge fitted cols to "wins_a", "losses_a", "wins_b", "losses_b"
+        df_new = X.copy()
+        df_new["wins_a"] = df_new[self.cols_wins_a].sum(axis=1)
+        df_new["losses_a"] = df_new[self.cols_losses_a].sum(axis=1)
+        df_new["wins_b"] = df_new[self.cols_wins_b].sum(axis=1)
+        df_new["losses_b"] = df_new[self.cols_losses_b].sum(axis=1)
+        # pass new DF to calculate_odds
+        df_new = calc_odds(df_new, self.n_samples, self.n_experiments)
+        # drop artificial columns
+        df_new.drop(["wins_a", "losses_a", "wins_b", "losses_b"], axis=1, inplace=True)
+        return df_new
